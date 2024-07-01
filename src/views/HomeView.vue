d3581e1afd60ada4aa46b7cebf8f17960536478b
Stephanie Odoom <styzyl18@gmail.com>
2024-07-01 19:55:37 +0000

    Creating a sample vue blog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
+    <p>My name is {{ name }} and age is {{ age }}</p>
+    <button @click="handleClick">Click</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log('clicked')
+    }
+
+    return {
+      name, age, handleClick
+    }
   }
 }
 </script>
